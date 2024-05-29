3ee0694d7a69474684c841cedf05013580a49407
tomy08 <tomasesantacruz@gmail.com>
2024-05-28 21:48:57 -0300

    🎉 first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// hangman game
+
+
+$words = array(
+    'apple' => 'a fruit',
+    'banana' => 'a fruit',
+    'Star Wars' => 'a science fiction movie',
+    'PHP' => 'a programming language',
+    'HTML' => 'a markup language',
+    'LED' => 'a light emitting diode',
+    'keyboard' => 'an input device',
+    'monitor' => 'an output device',
+    'mouse' => 'an input device',
+    'fruit' => 'a food',
+    'vegetable' => 'a food',
+    'cactus' => 'a plant',
+    'rose' => 'a flower',
+    'bed' => 'a furniture',
+    'table' => 'a furniture',
+    'chair' => 'a furniture',
+    'laptop' => 'a computer',
+    'shoes' => 'a footwear',
+    'socks' => 'a clothing',
+    'shirt' => 'a clothing',
+    'pants' => 'a clothing',
+    'hat' => 'a clothing'
+);
